3a97449dba907f8d4cc74fa68e864cd49cc69526
Adebayo Victor <dbvctr@gmail.com>
2024-07-10 15:16:02 -0700

    third commit

--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -1,12 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:hybkproject/appcolor.dart';
 import 'package:hybkproject/appimage.dart';
+import 'package:hybkproject/core.dart';
 import 'package:hybkproject/forgot_password.dart';
 import 'package:hybkproject/text_styles.dart';
-import 'package:hybkproject/upload_image_new.dart';
+import 'package:image_picker/image_picker.dart';
 
-class UploadImage extends StatelessWidget {
+class UploadImage extends StatefulWidget {
   const UploadImage({super.key});
 
+  @override
+  State<UploadImage> createState() => _UploadImageState();
+}
+
+class _UploadImageState extends State<UploadImage> {
+  File? image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,36 +26,69 @@ class UploadImage extends StatelessWidget {
             SizedBox(height: 200),
             Stack(
               children: [
-                Container(
-                  height: 150,
-                  width: 150,
-                  decoration:
-                      BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+                image != null
+                    ? CircleAvatar(
+                        radius: 80,
+                        backgroundImage: FileImage(image!),
+                      )
+                    : Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.grey),
+                      ),
+                Positioned(
+                  bottom: 3,
+                  right: 15,
+                  child: GestureDetector(
+                    onTap: () async {
+                      setState(() {
+                        image = null;
+                      });
+                      var img = await AppCore.pickImage(ImageSource.camera);
+                      if (img != null) {
+                        setState(() {
+                          image = img;
+                        });
+                      }
+                    },
+                    child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppColor.primaryColor),
+                        child: Image(image: AssetImage(Appimage.pencil))),
+                  ),
                 ),
-                Image(image: AssetImage(Appimage.pencil)),
               ],
             ),
             SizedBox(height: 40),
-            Text(
-              'Upload your profile picture',
-              style: AppTextStyle.header.copyWith(fontSize: 26),
-            ),
-            SizedBox(height: 20),
-            Text(
-              textAlign: TextAlign.center,
-              'Choose a profile image that represents you. \nThis will help others recognize you in \nthe 10dollar community.',
-              style: AppTextStyle.body(),
-            ),
+            image != null
+                ? Text(
+                    'Awesome! \nYour profile picture looks great.',
+                    textAlign: TextAlign.center,
+                    style: AppTextStyle.header.copyWith(fontSize: 20),
+                  )
+                : Column(
+                    children: [
+                      Text(
+                        'Upload your profile picture',
+                        style: AppTextStyle.header.copyWith(fontSize: 26),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        textAlign: TextAlign.center,
+                        'Choose a profile image that represents you. \nThis will help others recognize you in \nthe 10dollar community.',
+                        style: AppTextStyle.body(),
+                      ),
+                    ],
+                  ),
             SizedBox(height: 100),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: AppButton(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => UploadImageNew()));
-                },
+                onTap: () {},
               ),
             ),
             SizedBox(height: 40),
